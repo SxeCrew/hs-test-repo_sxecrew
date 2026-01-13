38c836393a37764549ab2bbb8e0fdf2c242f139c
Сергей Шеремет <sheremet.ss@edu.spbstu.ru>
2026-01-14 02:40:06 +0300

    job2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -1,6 +1,7 @@
 module Part2.Tasks where
 
 import Util(notImplementedYet)
+import Data.Semigroup (Min(Min))
 
 data BinaryOp = Plus | Minus | Times deriving (Show, Eq)
 
@@ -12,18 +13,53 @@ data Term = IntConstant { intValue :: Int }          -- числовая кон�
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
+(|/|) :: Term -> Term -> Term
+(|/|) = BinaryTerm Divide
+
+infixl 6 |+|
+
+infixl 6 |-|
+
+infixl 7 |*|
+
+infixl 7 |/|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement = go
+  where
+    go expr = case expr of
+      Variable name | name == varName -> replacement
+      Constant _ -> expr
+      BinaryTerm op left right -> 
+        BinaryTerm op (go left) (go right)
+      UnaryTerm op inner ->
+        UnaryTerm op (go inner)
+      _ -> expr
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate term = case term of
+  BinaryTerm op left right ->
+    let lhs = evaluate left
+        rhs = evaluate right
+    in case (lhs, rhs) of
+      (IntConstant l, IntConstant r) ->
+        case op of
+          Plus   -> IntConstant (l + r)
+          Minus  -> IntConstant (l - r)
+          Times  -> IntConstant (l * r)
+          Divide -> 
+            if r == 0 
+              then error "error divide 0" 
+              else IntConstant (l `div` r)
+      _ -> BinaryTerm op lhs rhs
+  
+  _ -> term
